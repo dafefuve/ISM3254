1f2927ad2b18420c244c2c1e3436a0d62522887b
Daniel Fuentes <daniel@Daniels-Mac-Pro.local>
2015-02-19 01:54:01 -0500

    minor name refactoring

--- Assignment9/src/assignment9/Assignment9.java
+++ Assignment9/src/assignment9/Assignment9.java
@@ -1,16 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication9;
+package assignment9;
 import java.util.Scanner;
 
 /**
  *
  * @author dafefuve
  */
-public class JavaApplication9 {
+public class Assignment9 {
 
     /**
      * @param args the command line arguments
