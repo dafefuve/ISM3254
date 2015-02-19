4344c2464bba95a8505ad924875c3a91bfeb9f64
dafefuve <dafefuve@DAFEFUVE-MACPRO.local>
2015-02-19 15:29:27 -0500

    assignment13 compleated

--- Assignment13/src/assignment13/assignment13.java
+++ Assignment13/src/assignment13/assignment13.java
@@ -4,4 +4,27 @@ package assignment13;
  * Created by dafefuve on 2/19/15.
  */
 public class assignment13 {
+    public static void main(String[] args) {
+        String[] stress = {"English", "Thai", "German", "Russian", "Dutch", "Persian"};
+        String[] syllable = {"French", "Spanish", "Chinese", "Hindi"};
+        String[] moral = {"Japanese", "Ganda", "Korean"};
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if(j == 0){
+                    System.out.print(stress[i]);
+                    System.out.print("\t\t");
+                }
+                else if (j==1){
+                    System.out.print(syllable[i]);
+                    System.out.print("\t\t");
+                }
+                else{
+                    System.out.print(moral[i]);
+                    System.out.print("\t\t");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
